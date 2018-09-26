396693a43e8f23f0c659c4f358081901845b542c
uditaagrawal <42441447+uditaagrawal@users.noreply.github.com>
2018-09-26 11:30:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-sudo docker build ./website/ -t webdeploy
-sudo docker run -t -d -p 3002:80 --name phpappnew webdeploy 
+sudo docker build ./website/ -t webdeploy1
+sudo docker run -t -d -p 3002:80 --name phpappnew1 webdeploy1 
